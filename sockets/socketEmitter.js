0599c17bb08b4abbdf424ba03d2f8cdd97544d9e
Eliot Chan <eliot.chan@hotmail.ca>
2016-07-06 18:44:16 -0400

    Add missing semicolons

--- sockets/socketEmitter.js
+++ sockets/socketEmitter.js
@@ -6,11 +6,11 @@
 
 		module.emit = function emit(room, eventType, data) {
 			io.to(room).emit(eventType, data);
-		}
+		};
 
-		module.emitStartSession = function emitStartSession(room, votes, restaurants) {
-			io.to(room).emit('started', votes, restaurants);
-		}
+		module.emitStartedToUser = function emitStartedToUser(userId, votes, restaurants) {
+			io.to(userId).emit('started', votes, restaurants);
+		};
 
 		module.emitError = function emitError(room, data) {
 			io.to(room).emit('error', data);
